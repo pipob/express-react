24fcb8e2e4b167e7f07a35dd0be56b4393e47a1d
Pipob Piyaaromrat <49221538+pipob@users.noreply.github.com>
2020-09-23 07:52:19 +0700

    Add files via upload

---
+++ build/precache-manifest.34a6cef119ee622b6e682e33ebbb844f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff73c923a2d1eca300d4bf84dfb30ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1da6a120f71bbe9f6c",
+    "url": "/static/css/main.aac87787.chunk.css"
+  },
+  {
+    "revision": "fd07c7645cbfc85e690c",
+    "url": "/static/js/2.e0765cd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0765cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1da6a120f71bbe9f6c",
+    "url": "/static/js/main.0a6f4dd4.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
